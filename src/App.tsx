951c44c50e0e1f2edfc9b2b378d4d20cf23c14c0
Sam Flowers <111446369+sflowers51@users.noreply.github.com>
2024-04-20 21:06:09 -0400

    set invalid routes to just display home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
 import About from './views/About';
 import Projects from './views/Projects';
-import ErrorPage from './views/ErrorPage';
+
 
 const App: React.FC = () => (
   <Router>
@@ -11,7 +11,7 @@ const App: React.FC = () => (
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
-      <Route path="*" element={<ErrorPage />} />
+      <Route path="*" element={<Home/>} />
     </Routes>
   </Router>
 );
