84e75143e98765824f112e58f62a4ee7b4eb9b8a
Sam Flowers <111446369+sflowers51@users.noreply.github.com>
2024-05-12 22:08:51 -0500

    updated responsiveness in education section of about poage

--- src/components/EducationCard.tsx
+++ src/components/EducationCard.tsx
@@ -2,16 +2,16 @@ import culogo from "../assets/img/culogo.png";
 import fhu from "../assets/img/fhu.png";
 const EducationCard = () => {
   return (
-    <div className="md:w-[900px] md:h-[400px] w-[450px] h-[250px] gap-3 grid grid-cols-2 grid-rows-2">
+    <div className="md:w-[900px] md:h-[400px] w-[350px] h-[250px] gap-3 grid grid-cols-2 grid-rows-2">
       <div className="bg-white rounded-lg items-center justify-center flex ">
-        <img src={culogo} className="md:w-[350px] p-5" />
+        <img src={culogo} className="md:w-[350px] w-[250px] p-5" />
       </div>
-      <div className="text-left text-lg md:text-2xl flex flex-col md:gap-5 justify-center items-start md:px-12">
+      <div className="text-left  md:text-2xl flex flex-col md:gap-5 justify-center items-start md:px-12">
         <p>Cumberland University</p>
         <p> A.S. General Science</p>
         <p> 2020-2022</p>
       </div>
-      <div className="text-left md:text-2xl text-lg flex flex-col md:gap-5 justify-center md:px-12 ">
+      <div className="text-left md:text-2xl flex flex-col md:gap-5 justify-center md:px-12 ">
         <p>Freed-Hardeman University</p>
         <p>B.S. Computer Science </p>
         <p className="md:text-xl md:block hidden">Focus in Information Systems</p>
